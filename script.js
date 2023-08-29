aab4656c9d201165e2296613846bbc979008db46
Katter 0pdilahi Varah <132547886+Marxabeeye@users.noreply.github.com>
2023-08-29 12:15:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Sticky Navigation Menu JS Code
+
 let nav = document.querySelector("nav");
 let scrollBtn = document.querySelector(".scroll-button a");
 console.log(scrollBtn);
@@ -14,7 +14,6 @@ window.onscroll = function() {
 
 }
 
-// Side NavIgation Menu JS Code
 let body = document.querySelector("body");
 let navBar = document.querySelector(".navbar");
 let menuBtn = document.querySelector(".menu-btn");
@@ -34,7 +33,6 @@ cancelBtn.onclick = function(){
   scrollBtn.style.pointerEvents = "auto";
 }
 
-// Side Navigation Bar Close While We Click On Navigation Links
 let navLinks = document.querySelectorAll(".menu li a");
 for (var i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("click" , function() {
